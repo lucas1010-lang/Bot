2e9698ff9064063598f569c9c3b62d8e6056bad0
lucas1010-lang <lladislau07@gmail.com>
2026-04-21 20:10:53 -0300

    Add command registration for Discord bot

--- index.js
+++ index.js
@@ -16,3 +16,28 @@ client.on('messageCreate', message => {
 });
 
 client.login(process.env.MTQ5MjAxOTM1NzQ5NTMzMjg4NQ.GhRu_r.Bx6bDoiL-tDdEOoLiyp2MM54M4XjVaAjDLjc1U);
+require('dotenv').config();
+const { REST, Routes, SlashCommandBuilder } = require('discord.js');
+
+const commands = [
+  new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('Responde com Pong!')
+].map(command => command.toJSON());
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+    console.log('Registrando comandos...');
+
+    await rest.put(
+      Routes.applicationCommands('1471703783909359840'),
+      { body: commands }
+    );
+
+    console.log('Comandos registrados!');
+  } catch (error) {
+    console.error(error);
+  }
+})();
